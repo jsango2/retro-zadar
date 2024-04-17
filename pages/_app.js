e0fea08e30fb257f77e44ecdb0f43b1b2c98a12e
jsango2 <jure.sango@gmail.com>
2024-04-17 12:29:52 +0200

    add GA

--- pages/_app.js
+++ pages/_app.js
@@ -12,10 +12,8 @@ import { AnimatePresence, domAnimation, LazyMotion, m } from "framer-motion";
 import { GoogleTagManager } from "@next/third-parties/google";
 import { useState } from "react";
 
-import { animations } from "../lib/animations";
 import "../styles/index.css";
 import "../styles/index2.css";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps, router }) {
   return (
