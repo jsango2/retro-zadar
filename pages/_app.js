a6cf030c28e69181c99770d7bec6481ada223daa
jsango2 <jure.sango@gmail.com>
2024-04-17 11:59:21 +0200

    add GA

--- pages/_app.js
+++ pages/_app.js
@@ -29,23 +29,6 @@ function MyApp({ Component, pageProps, router }) {
 
   return (
     <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-
-      <Script strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',{
-          page_path: window.location.pathname,
-          
-![image](https://cdn.sanity.io/images/dgsq0x0m/production/61f24596fb210134e5df6a1dfa285de0f24791cc-1549x601.png?w=450)});
-         `}
-      </Script>
-
       <div className="app-wrap">
         <LazyMotion features={domAnimation}>
           <AnimatePresence exitBeforeEnter={!exitBefore}>
