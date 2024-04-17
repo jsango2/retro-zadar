d5ed592118b13c1f22ece616c851ad650adee524
jsango2 <jure.sango@gmail.com>
2024-04-17 11:51:23 +0200

    add GA

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,7 @@ import { useState } from "react";
 import { animations } from "../lib/animations";
 import "../styles/index.css";
 import "../styles/index2.css";
+import Script from "next/script";
 
 const pages = [
   { href: "/", name: "Index" },
@@ -27,23 +28,42 @@ function MyApp({ Component, pageProps, router }) {
   const [exitBefore, setExitBefore] = useState(false);
 
   return (
-    <div className="app-wrap">
-      <LazyMotion features={domAnimation}>
-        <AnimatePresence exitBeforeEnter={!exitBefore}>
-          <m.div
-            key={router.route.concat(animation.name)}
-            className="page-wrap"
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            variants={animation.variants}
-            transition={animation.transition}
-          >
-            <Component {...pageProps} />
-          </m.div>
-        </AnimatePresence>
-      </LazyMotion>
-    </div>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',{
+          page_path: window.location.pathname,
+          
+![image](https://cdn.sanity.io/images/dgsq0x0m/production/61f24596fb210134e5df6a1dfa285de0f24791cc-1549x601.png?w=450)});
+         `}
+      </Script>
+
+      <div className="app-wrap">
+        <LazyMotion features={domAnimation}>
+          <AnimatePresence exitBeforeEnter={!exitBefore}>
+            <m.div
+              key={router.route.concat(animation.name)}
+              className="page-wrap"
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              variants={animation.variants}
+              transition={animation.transition}
+            >
+              <Component {...pageProps} />
+            </m.div>
+          </AnimatePresence>
+        </LazyMotion>
+      </div>
+    </>
   );
 }
 export default MyApp;
