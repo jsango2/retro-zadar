0cd4591a1b05fa3a27a5aa6e0b62ecb57fcc33c6
jsango2 <jure.sango@gmail.com>
2024-04-20 12:09:02 +0200

    add GA

--- pages/index.js
+++ pages/index.js
@@ -46,18 +46,8 @@ export default function IndexPage() {
           key="canonical"
         />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/fav1.svg" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/fav1.svg"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/fav1.svg"
-        />
+        <link rel="icon" type="image/png" href="/favicon/fav1.svg" />
+        <link rel="icon" type="image/png" href="/favicon/fav1.svg" />
 
         <meta name="twitter:card" content="summary_large_image" />
 
