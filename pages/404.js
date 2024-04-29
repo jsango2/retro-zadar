478f030efb6c6db1c7de87d19dc58518a2b597c0
jsango2 <jure.sango@gmail.com>
2024-04-29 23:26:07 +0200

    deleted layout and meta

--- pages/404.js
+++ pages/404.js
@@ -4,25 +4,27 @@ import Link from "next/link";
 
 // import Seo from "../components/seo"
 // import Bart from "../images/bart.png"
-const NotFoundPage = () => (
-  <>
-    {/* <Seo title="404: Not found" /> */}
-    <div className="container">
-      <div className="wrapSuccess">
-        <div>{/* <img src={Bart} width="100px" alt="bart" />{" "} */}</div>
-        <h1 style={{ textAlign: "center", marginTop: "140px" }}>
-          404: Stranica nije pronađena
-        </h1>
-        <p>Upisali ste adresu koja ne postoji</p>
+const NotFoundPage = () => {
+  return (
+    <>
+      {/* <Seo title="404: Not found" /> */}
+      <div className="container">
+        <div className="wrapSuccess">
+          <div>{/* <img src={Bart} width="100px" alt="bart" />{" "} */}</div>
+          <h1 style={{ textAlign: "center", marginTop: "140px" }}>
+            404: Stranica nije pronađena
+          </h1>
+          <p style={{ textAlign: "center", marginTop: "140px" }}>
+            Upisali ste adresu koja ne postoji
+          </p>
 
-        <div
-          style={{ marginTop: "30px", textDecoration: "none", color: "white" }}
-        >
-          <Link href="/">Vrati se</Link>
+          <div style={{ textAlign: "center", marginTop: "140px" }}>
+            <Link href="/">Vrati se</Link>
+          </div>
         </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default NotFoundPage;
